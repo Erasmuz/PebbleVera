cdba66b5498f34b5272d20bb57cb805d93bd95b9
Aaron <halfeatencake@gmail.com>
2015-09-24 21:22:13 -0400

    Fixed another spelling mistake

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 $submitButton = $('#submitButton');
 
 $submitButton.on('click', function() {
-  console.log("SUBMIT!");
-  
   var return_to = getQueryParam('return_to', 'pebblejs://close#');
-  document.location = return_to + encodedURIComponent(JSON.stringify(getAndStoreConfigData()));
+  document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
 });
 
 function getAndStoreConfigData() {
