9a582aa2fb1688a6fafdf17a86ed9bed8656ea7c
Aaron <halfeatencake@gmail.com>
2015-09-24 21:07:07 -0400

    Added main.js file

---
+++ main.js
@@ -0,0 +1,8 @@
+$submintButton = $('#submitButton');
+
+$submitButton.on('click', function() {
+  console.log("SUBMIT!");
+  
+  //var return_to = getQueryParam('return_to', 'pebblejs://close#');
+  //document.location = return_to + encodedURIComponent(JSON.stringify(getAndStoreConfigData()));
+});
